89497844ebe27379e67d82d5b887caad76d84773
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2025-02-01 08:36:18 +0000

    Basic test project

---
+++ test/RichardJM.Coverlet.Tests/Class1Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace RichardJM.Coverlet.Tests;
+
+[TestFixture]
+public class Class1Tests
+{
+  [Test]
+  public void Add()
+  {
+    var class1 = new Class1();
+    Assert.That(class1.Add(1, 2), Is.EqualTo(3));
+  }
+}
